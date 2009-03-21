376bce341d001afcd44a2158a256f992156a59c0
Dr Nic Williams <drnicwilliams@gmail.com>
2009-03-21 22:24:31 +0800

    NoBrowserSupportLogger instance used where the current browser has no in-built/plugin test harness

--- src/browser_logger.js
+++ src/browser_logger.js
@@ -5,16 +5,24 @@ JsUnitTest.Unit.BrowserLogger = {
 JsUnitTest.Unit.BrowserLogger.forCurrentBrowser = function() {
   if (this.logger) { return this.logger; }
   if (typeof fireunit != "undefined" && fireunit) { 
-    this.logger = new JsUnitTest.Unit.BrowserLogger.FirefoxFireunit();
+    this.logger = new JsUnitTest.Unit.BrowserLogger.FirefoxFireunitLogger();
+  } else {
+    this.logger = new JsUnitTest.Unit.BrowserLogger.NoBrowserSupportLogger();
   }
   return this.logger;
 };
 
-JsUnitTest.Unit.BrowserLogger.FirefoxFireunit = function() {
+// All the BrowserLogger instances have the same API, which is stubbed out for the 
+// case where the current browser has no in-built/plugin test harness
+JsUnitTest.Unit.BrowserLogger.NoBrowserSupportLogger = function() {};
+JsUnitTest.Unit.BrowserLogger.NoBrowserSupportLogger.prototype.finish = function(status, message) {};
+
+JsUnitTest.Unit.BrowserLogger.FirefoxFireunitLogger = function() {
   if (typeof fireunit == "undefined" || !fireunit) { throw "fireunit not available"; }
   
 };
 
-JsUnitTest.Unit.BrowserLogger.FirefoxFireunit.prototype.finish = function(status, message) {
+JsUnitTest.Unit.BrowserLogger.FirefoxFireunitLogger.prototype.finish = function(status, message) {
   fireunit.ok(!!status, message);
 };
+
--- src/jsunittest.js
+++ src/jsunittest.js
@@ -7,7 +7,8 @@ JsUnitTest.Version = '<%= APP_VERSION %>';
 <%= include 'prototype/template.js', 'prototype/event.js' %>
 
 <%= include 'logger.js', 'message_template.js', 'ajax.js' %>
+<%= include 'browser_logger.js' %>
 <%= include 'assertions.js' %>
 <%= include 'runner.js', 'test_case.js' %>
 
-Test = JsUnitTest
+Test = JsUnitTest;
